1074a3a01577cdc31cb90e3f52bb1a468a1851f1
Amulya <88301130+gangamamulya@users.noreply.github.com>
2022-03-25 17:58:59 -0700

    Create Middle_of_Linked_List.py

---
+++ Middle_of_Linked_List.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+import collections
+class Node:
+    def __init__(self, val=0, next=None):
+         self.val = v al
+         self.next = next
+class Solution(object): 
+    def __init__(self,nodes=None):
+        self.head = None
+        
+    def middleNode(self, head):
+        node = [head]
+        while node[-1].next:
+            node.append(node[-1].next)
+        return node[len(node)//2]
+        
